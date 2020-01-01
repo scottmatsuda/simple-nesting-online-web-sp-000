687da0760dc3ffef2a8bf7656a7d56507af5f5ee
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-01 00:09:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
      programmer_hash[:dennis_ritchie][:languages][0]
 end
 
-def adding_matz(programmer_hash)
+def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
