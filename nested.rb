e99cd2e0365751af8645d3cf475bcee3ce21b729
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-01 01:03:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 end
